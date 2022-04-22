fd52ab9b5dc1bacb4bb88d7dec3e5133f7f689ed
Alejandro Martín Fernández <alejandro.mf1002@gmail.com>
2022-04-22 10:29:09 +0200

    first commit

---
+++ app/src/main/java/com/alexmartin/buscagemas/MainActivity.java
@@ -0,0 +1,14 @@
+package com.alexmartin.buscagemas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
