9a5ba244ddc41a0f2cd0da2cc098c69d47ab36ea
WOLFCRAY <alex.pallango.hinojoza@gmail.com>
2022-05-14 15:14:00 +0200

    grid

--- app/src/main/java/com/alexmartin/buscagemas/MainActivity.java
+++ app/src/main/java/com/alexmartin/buscagemas/MainActivity.java
@@ -2,7 +2,9 @@ package com.alexmartin.buscagemas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openGame(View view) {
+        Intent intent = new Intent(MainActivity.this, Game.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 }
