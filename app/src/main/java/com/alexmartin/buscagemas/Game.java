26e1777029af7f282eb5059d9a9b4d563d8804a4
WOLFCRAY <alex.pallango.hinojoza@gmail.com>
2022-05-14 20:12:58 +0200

    background game finish

--- app/src/main/java/com/alexmartin/buscagemas/Game.java
+++ app/src/main/java/com/alexmartin/buscagemas/Game.java
@@ -1,14 +1,39 @@
 package com.alexmartin.buscagemas;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 
 public class Game extends AppCompatActivity {
 
+    AnimationDrawable anim;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        ConstraintLayout container = (ConstraintLayout) findViewById(R.id.contenedor);
+
+        anim = (AnimationDrawable) container.getBackground();
+        anim.setEnterFadeDuration(3000);
+        anim.setExitFadeDuration(1000);
+    }
+
+    // Starting animation:- start the animation on onResume.
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (anim != null && !anim.isRunning())
+            anim.start();
+    }
+
+    // Stopping animation:- stop the animation on onPause.
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (anim != null && anim.isRunning())
+            anim.stop();
     }
 }
