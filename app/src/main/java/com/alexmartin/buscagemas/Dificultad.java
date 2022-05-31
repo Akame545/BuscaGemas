4057242eed7f74cb5838688eb5b640deb463068d
WOLFCRAY <alex.pallango.hinojoza@gmail.com>
2022-05-31 22:20:37 +0200

    frontend dificultad example

--- app/src/main/java/com/alexmartin/buscagemas/Dificultad.java
+++ app/src/main/java/com/alexmartin/buscagemas/Dificultad.java
@@ -1,72 +1,97 @@
 package com.alexmartin.buscagemas;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.view.ContextMenu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.PopupMenu;
 import android.widget.Toast;
 
-public class Dificultad extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener{
-
-    Button normal, dificil, muyDificil;
+public class Dificultad extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dificultad);
 
-//        normal = findViewById(R.id.normal);
-//        registerForContextMenu(normal);
-
     }
 
     public void desplegableNormal(View view) {
-        PopupMenu popupMenu = new PopupMenu(this, view);
-        popupMenu.setOnMenuItemClickListener(this);
-        popupMenu.inflate(R.menu.opciones_normal);
-        popupMenu.show();
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(getLayoutInflater().inflate(R.layout.alert_dialog_dificultad, null));
+        builder.setPositiveButton("50 Minas", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                Toast.makeText(Dificultad.this, "50 minas seleccionada", Toast.LENGTH_SHORT).show();
 
+            }
+        });
+        builder.setNeutralButton("20 Minas", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                Toast.makeText(Dificultad.this, "20 minas seleccionada", Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setNegativeButton("35 Minas", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                Toast.makeText(Dificultad.this, "35 minas seleccionada", Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 
-    @Override
-    public boolean onMenuItemClick(MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.veinte:
-                Toast.makeText(this, "20 minas seleccionada", Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.treintaCinco:
-                Toast.makeText(this, "35 minas seleccionada", Toast.LENGTH_SHORT).show();
-                return true;
-            default:
-                Toast.makeText(this, "50 minas seleccionada", Toast.LENGTH_SHORT).show();
-                return false;
-        }
+    public void desplegableDificil(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(getLayoutInflater().inflate(R.layout.alert_dialog_dificultad, null));
+        builder.setPositiveButton("130 Minas", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                Toast.makeText(Dificultad.this, "130 minas seleccionada", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        builder.setNegativeButton("100 Minas", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                Toast.makeText(Dificultad.this, "100 minas seleccionada", Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
-    //    @Override
-//    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-//        super.onCreateContextMenu(menu, v, menuInfo);
-//        menu.setHeaderTitle("Elige cuantas minas quieres que haya:");
-//        getMenuInflater().inflate(R.menu.opciones_normal, menu);
-//    }
-//
-//    @Override
-//    public boolean onContextItemSelected(@NonNull MenuItem item) {
-//        switch (item.getItemId()){
-//            case R.id.veinte:
-//                Toast.makeText(this, "20 minas seleccionada", Toast.LENGTH_SHORT).show();
-//                break;
-//            case R.id.treintaCinco:
-//                Toast.makeText(this, "35 minas seleccionada", Toast.LENGTH_SHORT).show();
-//                break;
-//            default:
-//                Toast.makeText(this, "50 minas seleccionada", Toast.LENGTH_SHORT).show();
-//        }
-//
-//        return super.onContextItemSelected(item);
-//    }
+
+    public void desplegableMuyDificil(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(getLayoutInflater().inflate(R.layout.alert_dialog_dificultad, null));
+        builder.setPositiveButton("280 Minas", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                Toast.makeText(Dificultad.this, "280 minas seleccionada", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        builder.setNegativeButton("200 Minas", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                Toast.makeText(Dificultad.this, "200 minas seleccionada", Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
 }
